2cb68a1354f93edce28fbd763ed281b88669b49d
Antony Saputra <d00197615@student.dkit.ie>
2019-11-12 20:57:34 +0000

    Created CharacterClass to store character attributes. Added RigidBody, CapsuleCollider, and Physics Material on 2 spawned character(Troop). Created 2 empty GameObject for Player 1 and Player 2 as a placeholder

---
+++ BOTSS/Assets/Scripts/CharacterBase/CharacterStat.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterStat : MonoBehaviour
+{
+
+    public int health;
+    public int speed;
+    public int range;
+    public int attack;
+    public ClassID classID;
+    public PlayerID alliance;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
