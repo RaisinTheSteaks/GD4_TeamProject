1dac9d85edbf6a5d27b6c232fe0e864e2d164b05
joshuaCorcoran <44177838+joshuaCorcoran@users.noreply.github.com>
2019-11-14 19:05:55 +0000

    JC- Added trigger volume layer to map with Hexagon prefab object

---
+++ BOTSS/Assets/Prefab/Level/NavGrid/HexUnit.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HexUnit : MonoBehaviour
+{
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (true)
+        {
+            Debug.Log("entered");
+        }
+    }
+}
