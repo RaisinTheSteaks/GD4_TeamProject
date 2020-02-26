1a5732acbae7d8bb7b4606ecb446ccc9e4a39b64
Joshua Corcoran <joshuacorcoran03@outlook.com>
2020-02-26 20:21:38 +0000

    JC- Added cover class, includes some features that may get implemented later

---
+++ Vuforia/Assets/Scripts/CoverScripts/Cover.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public enum CoverType
+{
+    FullCover,
+    PartialCover,
+    CoverTypeCount
+}
+
+public class Cover : MonoBehaviour
+{
+    
+    public string ID
+    {
+        get
+        {
+            return id;
+        }
+    }
+
+    public HexCell parentCell
+    {
+        get
+        {
+            return cellPosition;
+        }
+        set
+        {
+            //Add the position offsets and orientation offsets here
+            this.cellPosition = value;
+        }
+    }
+
+
+
+    CoverType coverType;
+    string id;
+    int cellFace;
+    HexCell cellPosition;
+}
