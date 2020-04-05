76c8d986bf789b1ea7072a3439e1891dac83ba60
Antony Saputra <antonysaputra99@outlook.com>
2020-04-05 14:05:11 +0100

    AS - Create billboard for health bar

---
+++ Vuforia/Assets/Scripts/Billboard.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Billboard : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Camera camera;
+    void Start()
+    {
+        camera = GameObject.Find("ARCamera").GetComponent<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(transform.position + camera.transform.rotation * Vector3.back);
+    }
+}
