cf218473b899987c4db7ad7498f4759798f6ac85
Antony Saputra <antonysaputra99@outlook.com>
2020-04-13 20:34:29 +0100

    AS - add allied and enemy troopA

---
+++ Vuforia/Assets/test.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject bot;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+           Animator animator = bot.GetComponent<Animator>();
+           animator.SetBool("IsShooting", true);
+        }
+    }
+}
